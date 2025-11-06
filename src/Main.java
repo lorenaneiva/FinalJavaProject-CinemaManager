746494c53b54566fe2da84a0f578f295b75a1c80
Lorena Gama Neiva <lorenaneiva2232820@gmail.com>
2025-11-06 15:20:04 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/*
+    Classe principal do sistema de gerenciamento de cinema.
+    Responsável por iniciar os serviços e exibir o menu principal.
+*/
+
+public class Main {
+    public static void main(String[] args){
+        
+    }
+    
+    public static void exibirMenuPrincipal() {
+    
+    }
+}
+
