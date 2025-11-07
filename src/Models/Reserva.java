4b744ed17be574313d0fd1e8be3368f5f1fb87b3
daniel <danielfariaslogin@gmail>
2025-11-06 23:42:13 -0300

    adição da classe Reserva e adição da classe Assento

--- src/Models/Reserva.java
+++ src/Models/Reserva.java
@@ -2,13 +2,65 @@ package Models;
 
 public class Reserva {
     private Sessao sessao;
-    private int assento;
-    private String statusPagamento;
+    //adicionei o atributo Assento pois era não faz muito sentido um assento ser apenas um número sem verificação de ocupação
+    private Assento assento;
+    private boolean statusPagamento;
 
-    public Reserva (Sessao sessao, int assento, String statusPagamento){
+    public Reserva (Sessao sessao, Assento assento, boolean statusPagamento){
         this.sessao = sessao;
         this.assento = assento;
         this.statusPagamento = statusPagamento;
     }
 
+    public Sessao getSessao(){
+        return sessao;
+    }
+
+    public void setSessao(Sessao sessao){
+        if (sessao == null ){
+            throw new IllegalArgumentException("Precisa adicionar uma sessão para fazer reserva");
+        }
+        this.sessao = sessao;
+    }
+
+    public Assento getAssento(){
+        return assento;
+    }
+
+    public void setAssento(Assento assento){
+        if (assento == null){
+            throw new IllegalArgumentException("Precisa colocar uma assento para assistir o filme");
+        }
+        this.assento = assento;
+    }
+
+    public boolean isStatusPagamento(){
+        return statusPagamento;
+    }
+
+    public void setStatusPagamento(boolean statusPagamento){
+        this.statusPagamento = statusPagamento;
+    }
+
+    public void verificarStatusPagamento() {
+        if (statusPagamento) {
+            System.out.println("Pagamento confirmado");
+        } else {
+            System.out.println("Aguardando pagamento");
+        }
+    }
+
+    public void confirmarPagamento() {
+        this.statusPagamento = true;
+        System.out.println("Pagamento confirmado do assento: " + assento.getNumero());
+    }
+
+    @Override
+    public String toString() {
+        String status = statusPagamento ? "Pago" : "Pendente";
+        return "Resumo da reserva: "+ " | " + "Filme: " + sessao.getFilme().getTitulo()  + " |" + "Data da Sessão: " + sessao.getData() + " | " + "Horário da Sessão: " + sessao.getHorario() + " | " + "Sala: " +  sessao.getSala().getNome() +" | " + "Assento: " +  assento.getNumero() + " | " + "Status: " +  status;
+
+
+    }
+    
 }
