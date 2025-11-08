dc5f8eed5df536682fc13cc9989ca0379e04300d
feliped2422 <pf58744@gmail.com>
2025-11-08 15:27:50 -0300

    parte do Sessao Service terminada

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,13 @@
 */
 
 import Models.Filme;
+import cinema.services.SessaoService;
 
 public class Main {
     public static void main(String[] args){
         Filme obj1 = new Filme("A volta dos que nunca foram", "Terror", "18+", 120);
+         SessaoService sessaoService = new SessaoService();
+
         
         System.out.println(obj1.toString());
     }
