634a21d5f852a4164bb480b9b845fe962a739b60
Lorena Gama Neiva <lorenaneiva2232820@gmail.com>
2025-11-17 19:12:50 -0300

    organiza as pastas de arquivos de testes e coloca os métodos que estão dando conflito como comentários

--- src/Services/SessaoService.java
+++ src/Services/SessaoService.java
@@ -6,14 +6,14 @@ import Models.Sessao;
 public class SessaoService {
 
     //para adicionar um filme
-    public void adicionarFilme(Filme filme, Sessao sessao) {
-    if (filme == null) {
-        throw new IllegalArgumentException("O filme não pode ser nulo.");
-    }
-
-    sessao.listarFilmes().add(filme);
-    System.out.println("Filme '" + filme.getTitulo() + "' com ID " + filme.getId() + " adicionado com sucesso.");
-}
+//    public void adicionarFilme(Filme filme, Sessao sessao) {
+//    if (filme == null) {
+//        throw new IllegalArgumentException("O filme não pode ser nulo.");
+//    }
+//
+//    sessao.listarFilmes().add(filme);
+//    System.out.println("Filme '" + filme.getTitulo() + "' com ID " + filme.getId() + " adicionado com sucesso.");
+//}
 
     //para buscar um filme por ID
     public void definirFilmePorId(int id, Sessao sessao, FilmeService filmeService) {
@@ -41,14 +41,14 @@ public class SessaoService {
     }
 
     //para remover um filme
-    public boolean removerFilme(int id, Sessao sessao, FilmeService filmeService) {
-        Filme filmeEncontrado = filmeService.buscarFilmePorId(id);
-        if (filmeEncontrado != null) {
-            sessao.listarFilmes().remove(filmeEncontrado);
-            System.out.println("Filme com ID " + id + " removido com sucesso.");
-            return true;
-        } else {
-            throw new IllegalArgumentException("Filme com ID " + id + " não encontrado.");
-        }
-    }
+//    public boolean removerFilme(int id, Sessao sessao, FilmeService filmeService) {
+//        Filme filmeEncontrado = filmeService.buscarFilmePorId(id);
+//        if (filmeEncontrado != null) {
+//            sessao.listarFilmes().remove(filmeEncontrado);
+//            System.out.println("Filme com ID " + id + " removido com sucesso.");
+//            return true;
+//        } else {
+//            throw new IllegalArgumentException("Filme com ID " + id + " não encontrado.");
+//        }
+//    }
 }
