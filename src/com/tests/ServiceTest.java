a2d2e4ecaa86e710792c0ebee6288297d6bc5379
Lorena Gama Neiva <lorenaneiva2232820@gmail.com>
2025-11-17 19:14:49 -0300

    cria pasta para arquivos de testes

---
+++ src/com/tests/ServiceTest.java
@@ -0,0 +1,49 @@
+package com.tests;
+
+import Models.Filme;
+import Services.FilmeService;
+
+public class ServiceTest {
+    public static void main(String[] args){
+        FilmeService filmeService = new FilmeService();
+
+        Filme filme = new Filme();
+        filme.setTitulo("O Dr. House");
+        filme.setGenero("Medicina");
+        filme.setClassificacao("+16");
+        filme.setDuracao(90);
+
+        filmeService.adicionarFilme(filme);
+        
+        Filme filme2 = new Filme();
+        filme2.setTitulo("Homem aranha");
+        filme2.setGenero("Ficcao cientifica");
+        filme2.setClassificacao("+14");
+        filme2.setDuracao(120);
+        // adicionar filme
+        filmeService.adicionarFilme(filme2);
+            
+        System.out.println("----------");
+
+        System.out.println(filmeService.listarFilmes());
+        // editar filme
+        System.out.println("----------");
+
+        filmeService.editarFilme(1, "TituloTeste", "GeneroTeste", "ClassificaoTeste", 666);
+
+        System.out.println(filmeService.listarFilmes());
+        
+        // buscarFilmePorId
+        System.out.println("----------");
+        System.out.println(filmeService.buscarFilmePorId(1));
+
+        // remover filme  
+        System.out.println("----------");
+
+        filmeService.removerFilme(1);
+        
+        System.out.println(filmeService.listarFilmes());
+
+    }
+}
+
